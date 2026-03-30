237f439d975052729e0daa953cbba04fff5f9f1f
Hymavathi-Samsani <154891434+Hymavathi-Samsani@users.noreply.github.com>
2026-03-30 17:35:28 +0530

    Delete Get file info from the given path.py

--- Get file info from the given path.py
+++
@@ -1,8 +0,0 @@
-#Getting file information from given path
-
-path='abfss://hymaa_practice_2026@onelake.dfs.fabric.microsoft.com/lh_raw.Lakehouse/Files/'
-
-ls=mssparkutils.fs.ls(path) #Get files details from the path
-
-for i in ls: # Loop through files list array
-    print(i.name,i.path,i.size,i.isDir,i.isFile,i.modifyTime) # Print file info
