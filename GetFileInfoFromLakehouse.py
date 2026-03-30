ef253f5ee2746cb76a40e416b8ed2f99adfe588c
Hymavathi-Samsani <154891434+Hymavathi-Samsani@users.noreply.github.com>
2026-03-30 17:35:37 +0530

    Delete GetFileInfoFromLakehouse.py

--- GetFileInfoFromLakehouse.py
+++
@@ -1,8 +0,0 @@
-#Getting file information from given path
-
-path='abfss://hymaa_practice_2026@onelake.dfs.fabric.microsoft.com/lh_raw.Lakehouse/Files/'
-
-ls=mssparkutils.fs.ls(path)
-
-for i in ls:
-    print(i.name,i.path,i.size,i.isDir,i.isFile,i.modifyTime)
