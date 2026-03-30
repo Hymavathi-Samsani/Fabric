ec8d379b351c23451ec29ff0d7b78d8c5c401488
Hymavathi-Samsani <154891434+Hymavathi-Samsani@users.noreply.github.com>
2026-03-30 17:30:56 +0530

    Delete Removing only files from given path.py

--- Removing only files from given path.py
+++
@@ -1,8 +0,0 @@
-#Removing only files from given path
-
-path='abfss://hymaa_practice_2026@onelake.dfs.fabric.microsoft.com/lh_raw.Lakehouse/Files/'
-
-ls=mssparkutils.fs.ls(path+'Databricks/')
-for i in ls:
-    if i.isFile:
-        mssparkutils.fs.rm(i.path,True)
