11d48b195faa43372bafe2f9975e7ddcefcdb949
Hymavathi-Samsani <154891434+Hymavathi-Samsani@users.noreply.github.com>
2026-03-30 17:37:24 +0530

    Delete Copy only CSV file from given path to CSV path.py

--- Copy only CSV file from given path to CSV path.py
+++
@@ -1,9 +0,0 @@
-#Copy only CSV file from given path to CSV path
-
-path='abfss://hymaa_practice_2026@onelake.dfs.fabric.microsoft.com/lh_raw.Lakehouse/Files/'
-
-mssparkutils.fs.mkdirs(path+'CSV')
-ls=mssparkutils.fs.ls(path)
-for i in ls:
-    if i.isFile and i.name.endswith('.csv'):
-        mssparkutils.fs.cp(i.path,path+'CSV/')
